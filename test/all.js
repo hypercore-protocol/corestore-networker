63322f2c2456efbb6ccfd6bd628193e2fa54b81d
Andrew Osheroff <andrewosh@gmail.com>
2019-10-24 15:52:25 +0200

    Added tests and updated for corestore v4

--- test/all.js
+++ test/all.js
@@ -4,20 +4,91 @@ const Corestore = require('corestore')
 
 const SwarmNetworker = require('..')
 
-test.only('simple replication', async t => {
+test('simple replication', async t => {
   const { store: store1, networker: networker1 } = await create()
   const { store: store2, networker: networker2 } = await create()
 
   const core1 = await store1.get()
   const core2 = await store2.get(core1.key)
-  console.log('after cores')
+
   networker1.seed(core1.discoveryKey)
   networker2.seed(core2.discoveryKey)
 
   await append(core1, 'hello')
-  // This will fail if ifAvailable: true
-  const data = await get(core2, 0, { ifAvailable: false })
+  const data = await get(core2, 0)
   t.same(data, Buffer.from('hello'))
+
+  await cleanup([networker1, networker2])
+  t.end()
+})
+
+test('replicate multiple top-level cores', async t => {
+  const { store: store1, networker: networker1 } = await create()
+  const { store: store2, networker: networker2 } = await create()
+
+  const core1 = await store1.get()
+  const core2 = await store1.get()
+  const core3 = await store2.get(core1.key)
+  const core4 = await store2.get(core2.key)
+
+  networker1.seed(core1.discoveryKey)
+  networker1.seed(core2.discoveryKey)
+  networker2.seed(core2.discoveryKey)
+  networker2.seed(core3.discoveryKey)
+
+  await append(core1, 'hello')
+  await append(core2, 'world')
+  const d1 = await get(core3, 0)
+  const d2 = await get(core4, 0)
+  t.same(d1, Buffer.from('hello'))
+  t.same(d2, Buffer.from('world'))
+
+  await cleanup([networker1, networker2])
+  t.end()
+})
+
+test('replicate to multiple receivers', async t => {
+  const { store: store1, networker: networker1 } = await create()
+  const { store: store2, networker: networker2 } = await create()
+  const { store: store3, networker: networker3 } = await create()
+
+  const core1 = await store1.get()
+  const core2 = await store2.get(core1.key)
+  const core3 = await store3.get(core1.key)
+
+  networker1.seed(core1.discoveryKey)
+  networker2.seed(core2.discoveryKey)
+  networker3.seed(core3.discoveryKey)
+
+  await append(core1, 'hello')
+  const d1 = await get(core2, 0)
+  const d2 = await get(core3, 0)
+  t.same(d1, Buffer.from('hello'))
+  t.same(d2, Buffer.from('hello'))
+
+  await cleanup([networker1, networker2, networker3])
+  t.end()
+})
+
+test('replicate sub-cores', async t => {
+  const { store: store1, networker: networker1 } = await create()
+  const { store: store2, networker: networker2 } = await create()
+
+  const core1 = await store1.get()
+  const core3 = await store2.get(core1.key)
+
+  networker1.seed(core1.discoveryKey)
+  networker2.seed(core3.discoveryKey)
+
+  const core2 = await store1.get({ parents: [core1.key] })
+  const core4 = await store2.get({ key: core2.key, parents: [core3.key]})
+
+  await append(core1, 'hello')
+  await append(core2, 'world')
+  const d1 = await get(core3, 0)
+  const d2 = await get(core4, 0)
+  t.same(d1, Buffer.from('hello'))
+  t.same(d2, Buffer.from('world'))
 
   await cleanup([networker1, networker2])
   t.end()
@@ -46,9 +117,7 @@ function append (core, data) {
 
 function get (core, idx, opts = {}) {
   return new Promise((resolve, reject) => {
-    console.log('getting here')
     core.get(idx, opts, (err, data) => {
-      console.log('got err:', err, 'data:', data)
       if (err) return reject(err)
       return resolve(data)
     })
