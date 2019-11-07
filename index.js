5d942f13bc89bbeebfa89282f5f0ed33759adaf1
Andrew Osheroff <andrewosh@gmail.com>
2019-11-07 14:41:10 +0100

    Refactor for multiplexing

--- index.js
+++ index.js
@@ -28,31 +28,16 @@ class SwarmNetworker extends EventEmitter {
     this._swarm = null
   }
 
-  _handleConnection (socket, details) {
-    const isInitiator = !!details.client
-    var discoveryKey = details.peer ? details.peer.topic : null
-    var stream = null
-
-    if (isInitiator) {
-      // This is the active replication case -- we're requesting that a particular discovery key be replicated.
-      const dkeyString = datEncoding.encode(discoveryKey)
-      if (!this._seeding.has(dkeyString)) {
-        return socket.destroy(new Error('Networker is not seeding: ' + dkeyString))
-      }
-      stream = this.corestore.replicate(true, discoveryKey, { ...this._replicationOpts })
-      this._addStream(dkeyString, stream)
-    } else {
-      // This is the passive replication case -- we need to wait for the active peer to create a channel.
-      stream = new HypercoreProtocol(false)
-      stream.once('discovery-key', dkey => {
-        this.corestore.replicate(false, dkey, { ...this._replicationOpts, stream })
-        this._addStream(datEncoding.encode(dkey), stream)
-      })
-    }
-
-    return pump(socket, stream, socket, err => {
-      if (err) this.emit('replication-error', err)
+  _handleTopic (protocolStream, discoveryKey) {
+    // This is the active replication case -- we're requesting that a particular discovery key be replicated.
+    const dkeyString = datEncoding.encode(discoveryKey)
+    if (!this._seeding.has(dkeyString)) return
+    // The initiator parameter here is ignored, since we're passing in a stream.
+    const stream = this.corestore.replicate(false, discoveryKey, {
+      ...this._replicationOpts,
+      stream: protocolStream
     })
+    this._addStream(dkeyString, stream)
   }
 
   _addStream (discoveryKey, stream) {
@@ -63,23 +48,56 @@ class SwarmNetworker extends EventEmitter {
     }
     streams.push(stream)
 
+    /*
     stream.on('finish', onclose)
     stream.on('end', onclose)
     stream.on('close', onclose)
+    */
     var closed = false
 
     function onclose () {
       if (closed) return
+      console.log('STREAM IS CLOSING')
       streams.splice(streams.indexOf(stream), 1)
       closed = true
     }
   }
 
   listen () {
-    this._swarm = hyperswarm(this.opts)
+    this._swarm = hyperswarm({
+      ...this.opts,
+      queue: { multiplex: true }
+    })
     this._swarm.on('error', err => this.emit('error', err))
-    this._swarm.on('connection', (socket, details) => {
-      this._handleConnection(socket, details)
+    this._swarm.on('connection', (socket, info) => {
+      const isInitiator = !!info.client
+      console.log(this.id, 'INFO PEER', info.peer)
+      console.log('socket remoteaddr:', socket.remoteAddress)
+      if (socket.remoteAddress === '::ffff:127.0.0.1' || socket.remoteAddress === '127.0.0.1') return
+
+      console.log('info.topics.length:', info.topics.length)
+      const protocolStream = new HypercoreProtocol(isInitiator)
+      for (const discoveryKey of info.topics) {
+        this._handleTopic(protocolStream, discoveryKey)
+      }
+      info.on('topic', discoveryKey => {
+        console.log('TOPIC:', discoveryKey)
+        this._handleTopic(protocolStream, discoveryKey)
+      })
+      /*
+      protocolStream.on('close', () => {
+        console.log('PROTOCOL STREAM IS CLOSING')
+      })
+      protocolStream.on('error', err => {
+        console.error('PROTOCOL STREAM ERROR:', err)
+      })
+      */
+      protocolStream.on('discovery-key', discoveryKey => {
+        this._handleTopic(protocolStream, discoveryKey)
+      })
+      return pump(socket, protocolStream, socket, err => {
+        if (err) this.emit('replication-error', err)
+      })
     })
   }
 
@@ -90,6 +108,7 @@ class SwarmNetworker extends EventEmitter {
     const keyString = (typeof discoveryKey === 'string') ? discoveryKey : datEncoding.encode(discoveryKey)
     const keyBuf = (discoveryKey instanceof Buffer) ? discoveryKey: datEncoding.decode(discoveryKey)
 
+    console.log(this.id, 'SEEDING KEYSTRING:', keyString)
     this._seeding.add(keyString)
     this._swarm.join(keyBuf, {
       announce: true,
