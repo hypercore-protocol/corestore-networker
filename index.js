233014a26e3652d72d823470fc00dc0f94ef56d9
Andrew Osheroff <andrewosh@gmail.com>
2019-11-13 15:24:37 +0100

    Lots of cleanups + hypercore 8 updates

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const pump = require('pump')
 
 const log = require('debug')('corestore:network')
 
+const OUTER_STREAM = Symbol('corestore-outer-stream')
+
 class SwarmNetworker extends EventEmitter {
   constructor (corestore, opts = {}) {
     super()
@@ -22,7 +24,8 @@ class SwarmNetworker extends EventEmitter {
     }
 
     this._seeding = new Set()
-    this._replicationStreams = new Map()
+    this._replicationStreams = []
+    this._streamsByDiscoveryKey = new Map()
 
     // Set in listen
     this._swarm = null
@@ -33,34 +36,16 @@ class SwarmNetworker extends EventEmitter {
     const dkeyString = datEncoding.encode(discoveryKey)
     if (!this._seeding.has(dkeyString)) return
     // The initiator parameter here is ignored, since we're passing in a stream.
-    const stream = this.corestore.replicate(false, discoveryKey, {
+    this.corestore.replicate(false, discoveryKey, {
       ...this._replicationOpts,
       stream: protocolStream
     })
-    this._addStream(dkeyString, stream)
-  }
-
-  _addStream (discoveryKey, stream) {
-    var streams = this._replicationStreams.get(discoveryKey)
-    if (!streams) {
-      streams = []
-      this._replicationStreams.set(discoveryKey, streams)
+    var discoveryKeyStreams = this._streamsByDiscoveryKey.get(dkeyString)
+    if (!discoveryKeyStreams) {
+      discoveryKeyStreams = []
+      this._streamsByDiscoveryKey.set(dkeyString, discoveryKeyStreams)
     }
-    streams.push(stream)
-
-    /*
-    stream.on('finish', onclose)
-    stream.on('end', onclose)
-    stream.on('close', onclose)
-    */
-    var closed = false
-
-    function onclose () {
-      if (closed) return
-      console.log('STREAM IS CLOSING')
-      streams.splice(streams.indexOf(stream), 1)
-      closed = true
-    }
+    discoveryKeyStreams.push(protocolStream)
   }
 
   listen () {
@@ -71,48 +56,47 @@ class SwarmNetworker extends EventEmitter {
     this._swarm.on('error', err => this.emit('error', err))
     this._swarm.on('connection', (socket, info) => {
       const isInitiator = !!info.client
-      console.log(this.id, 'INFO PEER', info.peer)
-      console.log('socket remoteaddr:', socket.remoteAddress)
-      if (socket.remoteAddress === '::ffff:127.0.0.1' || socket.remoteAddress === '127.0.0.1') return
+      if (socket.remoteAddress === '::ffff:127.0.0.1' || socket.remoteAddress === '127.0.0.1') return null
 
-      console.log('info.topics.length:', info.topics.length)
-      const protocolStream = new HypercoreProtocol(isInitiator)
+      const protocolStream = new HypercoreProtocol(isInitiator, {
+        onchannelclose: (discoveryKey) => {
+          const dkeyString = datEncoding.encode(discoveryKey)
+          const streams = this._streamsByDiscoveryKey.get(dkeyString)
+          if (!streams || !streams.length) return
+          streams.splice(streams.indexOf(protocolStream), 1)
+          if (!streams.length) this._streamsByDiscoveryKey.delete(dkeyString)
+        },
+        ondiscoverykey: (discoveryKey) => {
+          this._handleTopic(protocolStream, discoveryKey)
+        }
+      })
+      this._replicationStreams.push(protocolStream)
+
       for (const discoveryKey of info.topics) {
         this._handleTopic(protocolStream, discoveryKey)
       }
       info.on('topic', discoveryKey => {
-        console.log('TOPIC:', discoveryKey)
         this._handleTopic(protocolStream, discoveryKey)
       })
-      /*
-      protocolStream.on('close', () => {
-        console.log('PROTOCOL STREAM IS CLOSING')
-      })
-      protocolStream.on('error', err => {
-        console.error('PROTOCOL STREAM ERROR:', err)
-      })
-      */
-      protocolStream.on('discovery-key', discoveryKey => {
-        this._handleTopic(protocolStream, discoveryKey)
-      })
+
       return pump(socket, protocolStream, socket, err => {
         if (err) this.emit('replication-error', err)
+        this._replicationStreams.splice(this._replicationStreams.indexOf(protocolStream), 1)
       })
     })
   }
 
-  seed (discoveryKey) {
+  seed (discoveryKey, opts = {}) {
     if (!this._swarm) throw new Error('Seed can only be called after the swarm is created.')
     if (this._swarm.destroyed) return
 
     const keyString = (typeof discoveryKey === 'string') ? discoveryKey : datEncoding.encode(discoveryKey)
     const keyBuf = (discoveryKey instanceof Buffer) ? discoveryKey: datEncoding.decode(discoveryKey)
 
-    console.log(this.id, 'SEEDING KEYSTRING:', keyString)
     this._seeding.add(keyString)
     this._swarm.join(keyBuf, {
-      announce: true,
-      lookup: true
+      announce: opts.announce !== false,
+      lookup: opts.lookup !== false
     })
   }
 
@@ -126,21 +110,24 @@ class SwarmNetworker extends EventEmitter {
     this._seeding.delete(keyString)
     this._swarm.leave(keyBuf)
 
-    const streams = this._replicationStreams.get(keyString)
+    const streams = this._streamsByDiscoveryKey.get(keyString)
     if (!streams || !streams.length) return
 
     for (let stream of [...streams]) {
-      stream.destroy()
+      stream.close(keyBuf)
     }
 
-    this._replicationStreams.delete(keyString)
+    this._streamsByDiscoveryKey.delete(keyString)
   }
 
   async close () {
     if (!this._swarm) return null
     return new Promise((resolve, reject) => {
-      for (let [dkey,] of new Map(...[this._replicationStreams])) {
-        if (dkey) this.unseed(datEncoding.decode(dkey))
+      for (const dkey of [...this._seeding]) {
+        this.unseed(dkey)
+      }
+      for (const stream of this._replicationStreams) {
+        stream.destroy()
       }
       this._swarm.destroy(err => {
         if (err) return reject(err)
